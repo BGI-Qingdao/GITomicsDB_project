78b88e6cab91472d537ec12b8edcfaff23896321
Super cat <gaoxiaomin1@genomics.cn>
2026-05-22 10:03:41 +0800

    Refactor SAMap script for improved readability
    Refactor argument parsing and streamline imports. Update CPU usage default and remove unused code.

--- 02.scRNA_3strategies_integration/01.SAMap.py
+++ 02.scRNA_3strategies_integration/01.SAMap.py
@@ -1,55 +1,48 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 25 08:06:34 2022
-
-@author: Ray
-"""
 
 import argparse as arg
 from samap.mapping import SAMAP
-from samap.analysis import (get_mapping_scores, GenePairFinder,
-                                sankey_plot, chord_plot, CellTypeTriangles,
-                                ParalogSubstitutions, FunctionalEnrichment,
-                                convert_eggnog_to_homologs, GeneTriangles)
-from samalg import SAM
-import pandas as pd
-import matplotlib.pyplot as plt
+from samap.analysis import get_mapping_scores, GenePairFinder
 import scanpy as sc
-import pickle
-import os
-
-parser = arg.ArgumentParser(formatter_class=arg.RawDescriptionHelpFormatter, description = '''This programe is used to perform SAMap.''')
-parser.add_argument('-mappath', action='store', help='Input blast path.')
-parser.add_argument('-species1', action='store', help='Input expression list.')
-parser.add_argument('-species2', action='store', help='Output expression matrix.')
-parser.add_argument('-fn1', action='store', help='Species h5ad')
-parser.add_argument('-fn2', action='store', help='Species h5ad')
-parser.add_argument('-celltype1', action='store', help='Celltype1.')
-parser.add_argument('-celltype2', action='store', help='Celltype2.')
-parser.add_argument('-NUMITERS', type=int, default=3, action='store', help='NUMITERS.')
-parser.add_argument('-cpu', type=int, default=20, help='Number of cpu used.')
-parser.add_argument('-mappingtable', action='store', help='Mappingtable path.')
-parser.add_argument('-enrichedGene', action='store', help='Enriched genes table')
+import pandas as pd
+parser = arg.ArgumentParser()
+parser.add_argument('-mappath')
+parser.add_argument('-species1')
+parser.add_argument('-species2')
+parser.add_argument('-fn1')
+parser.add_argument('-fn2')
+parser.add_argument('-celltype1')
+parser.add_argument('-celltype2')
+parser.add_argument('-NUMITERS', type=int, default=3)
+parser.add_argument('-cpu', type=int, default=5)
+parser.add_argument('-mappingtable')
+parser.add_argument('-enrichedGene')
 args = parser.parse_args()
+# read h5ad
+adata1 = sc.read_h5ad(args.fn1)
+adata2 = sc.read_h5ad(args.fn2)
+# ensure unique gene names
+adata1.var_names_make_unique()
+adata2.var_names_make_unique()
+# species files
+filenames = {args.species1: args.fn1,args.species2: args.fn2}
+# celltype keys
+keys = {args.species1: args.celltype1,args.species2: args.celltype2}
+# run SAMAP
+sm = SAMAP(filenames,keys=keys,f_maps=args.mappath,save_processed=False)
+sm.run(NUMITERS=args.NUMITERS,ncpus=args.cpu)
+# mapping scores
+D, MappingTable = get_mapping_scores(sm,keys,n_top=0)
+MappingTable.to_csv(args.mappingtable,sep='\t')
+# save UMAP
+def save_umap(fname, data):
+  df=pd.DataFrame({'samap_1': data[:,0],'samap_2': data[:,1]})
+  df.to_csv(fname,sep='\t',index=False)
+save_umap(f'{args.species1}.samap.csv',sm.sams[args.species1].adata.obsm['X_umap_samap'])
+save_umap(f'{args.species2}.samap.csv',sm.sams[args.species2].adata.obsm['X_umap_samap'])
+# enriched genes
+gpf = GenePairFinder(sm,keys=keys)
 
-fname = args.species1+args.species2+'.sm'
+gene_pairs = gpf.find_all(align_thr=0.1,thr=0.05)
 
-filenames = {args.species1:args.fn1, args.species2:args.fn2}
-keys={args.species1:args.celltype1, args.species2:args.celltype2}
-if not os.path.exists(fname):
-    print(args.mappath)
-    sm = SAMAP(
-        filenames,
-        keys=keys,
-        f_maps = args.mappath,
-        save_processed=False,
-       )
-    sm.run(NUMITERS=args.NUMITERS, ncpus=args.cpu)
-    #with open(args.species1+args.species2+'.sm', 'wb') as f:
-    #    pickle.dump(sm, f)
-else:
-    f = open(fname, 'rb')
-    sm = pickle.load(f)
-
-D,MappingTable=get_mapping_scores(sm, keys, n_top=0)
-MappingTable.to_csv(args.mappingtable, sep='\t')
+gene_pairs.to_csv(args.enrichedGene,sep='\t',index=False)
